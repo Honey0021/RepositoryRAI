2740c5af0226bcd47211e70f54b8957a6c7854ff
honey kn <honeyhari0021@gmail.com>
2023-03-17 17:44:39 +0530

    First commit

---
+++ MavenExample/src/test/java/PracticeMaven/MavenExample/RESTAPIAssuredTest.java
@@ -0,0 +1,22 @@
+package PracticeMaven.MavenExample;
+
+import org.testng.annotations.Test;
+
+public class RESTAPIAssuredTest {
+
+	@Test
+	public void PostJira() {
+	
+		System.out.println("PostJira");
+		
+		System.out.println("this line is changed by x person in develop branch ");
+		
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		
+		System.out.println("deleteTwitter");
+	}
+
+}
